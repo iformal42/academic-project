f94c8b2b88ad8477d9c53fd3bd8e1e7d83b9da45
iformal42 <bangimusaif2002@gmail.com>
2024-03-03 23:11:22 +0530

    add idle animation and left and right movement

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pygame as pg
+from player import Player
 
 # initializing constants
 FPS = 60
 SKY = (135, 206, 235)
 WIDTH, HEIGHT = 1200, 1000
+VELOCITY = 5
+
+
+def action_handler(p):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_d]:
+            p.move(VELOCITY)
+        if keys[pg.K_a]:
+            p.move(-VELOCITY)
 
 
 def main_game():
@@ -15,14 +25,23 @@ def main_game():
     # window
     window = pg.display.set_mode((WIDTH, HEIGHT))
 
+    # adding player object
+    player = Player(32, 32, window)
     running = True
     while running:
         # 60 FPS
+        clock.tick(60)
         window.fill(SKY)
+        # window.blit(player.stand(3, 2), (50, 500))
+        player.animation_idle(9)
         for event in pg.event.get():
             # stop condition
             if event.type is pg.QUIT:
                 running = False
+
+            action_handler(p=player)
+
+
         pg.display.update()
 
 
--- player.py
+++ player.py
@@ -1,21 +1,42 @@
 import pygame as pg
 
 PATH = "gameasset/Main Characters/Mask Dude/"
+JUMP = "Double Jump (32x32).png"
+IDLE = "Idle (32x32).png"
+RUN = "Run (32x32).png"
 
 
 class Player(pg.sprite.Sprite):
-    def __init__(self, width, height):
+    def __init__(self, width, height, screen):
         super().__init__()
-        self.player = []
-        self.image = pg.Surface((width, height))
+        self.x_vel, self.y_vel = 0, 0
+        self.width, self.height = width, height
+        self.window = screen
+        self.direction = "right"
+        self.character_img = pg.image.load(f"{PATH}{IDLE}")
+        self.rect = pg.Rect(0, 0, self.width, self.height)
+        self.player_list = []
+        self.animation()
+        self.animation_rate = 0
 
-        self.character_img = pg.image.load(f"{PATH}Idle (32x32).png")
-        self.rect1 = self.character_img.get_rect()
+    def stand(self, frame):
+        image = pg.Surface((self.width, self.height), pg.SRCALPHA).convert_alpha()
+        self.rect = pg.Rect(frame * self.width, 0, self.width, self.height)
+        image.blit(self.character_img, (0, 0), self.rect)
+        return pg.transform.scale2x(image)
 
-    def stand(self, width, height):
-        image = pg.Surface((width, height))
+    def animation(self):
+        for x in range(12):
+            self.player_list.append(self.stand(x))
 
+    def animation_idle(self, position):
+        self.animation_rate += 0.20
+        if int(self.animation_rate) >= 11:
+            self.animation_rate = 0
+        self.window.blit(self.player_list[int(self.animation_rate)], (self.rect.x, self.rect.y))
 
+    def move(self, horizontal_vel):
+        self.rect.move_ip(horizontal_vel, self.y_vel)
 
 
 if __name__ == "__main__":
@@ -23,22 +44,32 @@ if __name__ == "__main__":
     pg.init()
 
     clock = pg.time.Clock()
-    player = Player(50,50)
 
     # window
     window = pg.display.set_mode((1200, 1000))
 
+    player = Player(32, 32, window)
+    sprite = pg.sprite.Group()
+    # print(player)
+    # print(sprite)
+    # sprite.add(player)
     running = True
     while running:
         # 60 FPS
+        clock.tick(60)
+
         window.fill((135, 206, 235))
-        window.blit(player.image,(0,0))
 
-        # pg.draw.rect(window, (255, 0, 0), player.rect1)
-        # window.blit(player.character_img, (0, 0))
-
+        player.animation_idle(9)
+        # sprite.draw(window)
         for event in pg.event.get():
             # stop condition
             if event.type is pg.QUIT:
                 running = False
+
+            keys = pg.key.get_pressed()
+            if keys[pg.K_RIGHT]:
+                print("yes")
+                player.move()
+
         pg.display.update()
